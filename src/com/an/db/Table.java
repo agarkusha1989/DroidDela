afbca014648d95dce3d1591fe3a2d70754ec9bc0
Anatoly Garkusha <agarkusha1989@gmail.com>
2014-06-15 22:41:56 +0400

    Changes in Table interface clear imports and format source

--- src/com/an/db/Table.java
+++ src/com/an/db/Table.java
@@ -1,15 +1,21 @@
 package com.an.db;
 
-import android.content.ContentValues;
-
 public interface Table<K, R> {
 	public R select(K key);
+
 	public R[] select();
+
 	public R insert(R record);
+
 	public R[] insert(R[] records);
+
 	public R update(R record);
+
 	public R[] update(R[] records);
+
 	public void delete(R record);
+
 	public void delete(R[] records);
-	public void clear(); 
+
+	public void clear();
 }
