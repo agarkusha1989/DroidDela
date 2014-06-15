be26b7d05c4a6c07f8ea03767fcd565c83a493ce
Anatoly Garkusha <agarkusha1989@gmail.com>
2014-06-15 21:55:11 +0400

    Adding activity Main

---
+++ src/com/an/dela/activities/Main.java
@@ -0,0 +1,16 @@
+package com.an.dela.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.an.dela.R;
+
+public class Main extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+}
