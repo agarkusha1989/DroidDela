3bb5b9dac29365149ce8e6e28ad38e6208a53d76
Anatoly Garkusha <agarkusha1989@gmail.com>
2014-06-16 11:17:31 +0400

    Adding Broadcast Receiver OnAlarmTask

---
+++ src/com/an/dela/receivers/OnAlarmTask.java
@@ -0,0 +1,14 @@
+package com.an.dela.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnAlarmTask extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+	}
+
+}
