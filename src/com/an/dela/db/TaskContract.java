b14953cf560a0ab533f64145d98c378f439aa439
Anatoly Garkusha <agarkusha1989@gmail.com>
2014-06-16 10:25:23 +0400

    Bugfix in SQL_CREATE_TABLE

--- src/com/an/dela/db/TaskContract.java
+++ src/com/an/dela/db/TaskContract.java
@@ -11,7 +11,7 @@ public class TaskContract {
 	public static final String COLUMN_PRIORITY = "task_priority";
 	public static final String COLUMN_STATUS = "task_status";
 
-	public static final String SQL_CREATE_TABLE = "create table " + TABLE_NAME
+	public static final String SQL_CREATE_TABLE = "create table " + TABLE_NAME + " ("
 			+ COLUMN_ID + " integer primary key autoincrement, " 
 			+ COLUMN_TITLE + " text not null, " 
 			+ COLUMN_DATETIME + " datetime not null, "
