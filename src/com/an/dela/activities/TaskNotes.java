f4345ad71542b94326c1ee994015cdfac0900c10
Anatoly Garkusha <agarkusha1989@gmail.com>
2014-06-16 17:30:08 +0400

    Adding task notes function

--- src/com/an/dela/activities/TaskNotes.java
+++ src/com/an/dela/activities/TaskNotes.java
@@ -2,10 +2,13 @@ package com.an.dela.activities;
 
 import com.an.dela.R;
 import com.an.dela.db.Db;
+import com.an.dela.db.TaskContract;
 import com.an.dela.db.TaskRecord;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,6 +18,7 @@ public class TaskNotes extends Activity {
 	public static final String EXTRA_TASK_ID = "package com.an.dela.activities.EXTRA_TASK_ID";
 
 	private TextView titleTextView;
+	private EditText notesEditText;
 	private LinearLayout rootView;
 	
 	private TaskRecord taskRecord;
@@ -25,6 +29,7 @@ public class TaskNotes extends Activity {
 		setContentView(R.layout.activity_task_notes);
 		
 		titleTextView = (TextView) findViewById(R.id.title);
+		notesEditText = (EditText) findViewById(R.id.notes);
 		rootView = (LinearLayout) findViewById(R.id.task_notes);
 		
 		if (!loadTask()) {
@@ -35,6 +40,14 @@ public class TaskNotes extends Activity {
 		}
 	}
 	
+	public void onClickSaveButton(View view) {
+		String notes = notesEditText.getText().toString();
+		
+		taskRecord.set(TaskContract.COLUMN_NOTES, notes);
+		
+		taskRecord = Db.getInstance(this).getTaskTable().update(taskRecord);
+	}
+	
 	private boolean loadTask() {
 		int id = getIntent().getIntExtra(EXTRA_TASK_ID, -1);
 		
@@ -43,6 +56,8 @@ public class TaskNotes extends Activity {
 			return false;
 		
 		titleTextView.setText(taskRecord.getTitle());
+		notesEditText.setText(taskRecord.getNotes());
+		notesEditText.setSelection(notesEditText.getText().length());
 		int colorResId;
 		
 		if (taskRecord.equalPriority(TaskRecord.PRIORITY_HIGH)) {
